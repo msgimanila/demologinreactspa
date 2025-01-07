047889fb8253a148979c80869b2d131cc85ab9be
Mario Garcia <msgimanila@yahoo.com>
2025-01-08 06:53:06 +0800

    Update store.ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import reducer from './reducer';
 
+// Configure store
 const store = configureStore({
   reducer,
 });
+
+// Infer the RootState type from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+
+// Infer the AppDispatch type from the store's dispatch method
+export type AppDispatch = typeof store.dispatch;
 
 export default store;
