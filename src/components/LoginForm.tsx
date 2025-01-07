9f8399d5cfe61849ac0abd3b6125467fef8ee81b
Mario Garcia <msgimanila@yahoo.com>
2025-01-08 07:04:30 +0800

    Update LoginForm.tsx

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { loginRequest, loginSuccess, loginFailure } from '../redux/actions';
+import { RootState } from '../redux/store'; 
 
 const LoginForm = () => {
   const [username, setUsername] = useState('');
